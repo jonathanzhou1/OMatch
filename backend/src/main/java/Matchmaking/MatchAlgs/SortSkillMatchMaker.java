50a17589b5a4e825e23b2ed6b25e185d08150598
JMS100 <98605056+JMS100@users.noreply.github.com>
2023-12-08 11:36:46 -0500

    Feat: Added skill based matchmaking. Also made matchmaking for multiple teams at once, rather than just two teams

--- backend/src/main/java/Matchmaking/MatchAlgs/SortSkillMatchMaker.java
+++ backend/src/main/java/Matchmaking/MatchAlgs/SortSkillMatchMaker.java
@@ -4,22 +4,26 @@ import Matchmaking.Match;
 import Matchmaking.Player;
 import Matchmaking.Team;
 import java.io.IOException;
+import java.sql.Array;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
-public class SimpleMatchMaker implements IMatchMaker {
+public class SortSkillMatchMaker implements IMatchMaker{
 
   /**
-   * Simple matchmaking algorithm, splits up players alternatively without taking into account
-   * skill. Then pairs teams together
+   * Skill absed matchmaking algorithm, divides player alternatively after sorting on skill
+   * Then sort teams based on average skill and match
    * @param players, a list of players who will play in the match
    * @return
    */
+  @Override
   public List<Match> matchmaker(List<Player> players, int numTeams) throws IOException {
     if(numTeams % 2 != 0) {
       throw new IOException("Number of Teams Cannot Be Odd");
     }
-    int teamSize = players.size() / numTeams;
+    Collections.sort(players, (p1, p2) -> Float.compare(p1.getSkillLevel(), p2.getSkillLevel()));
     List<Team> teams = new ArrayList<>();
     for (int i = 0; i < numTeams; i++) {
       teams.add(new Team());
@@ -27,11 +31,10 @@ public class SimpleMatchMaker implements IMatchMaker {
     for (int i = 0; i < players.size(); i++) {
       teams.get(i % numTeams).addPlayer(players.get(i));
     }
-    List<Match> matches = new ArrayList<>();
-    for (int i = 0; i < numTeams; i += 2) {
+    Collections.sort(teams, (t1, t2) -> Float.compare(t1.getAvgSkill(), t2.getAvgSkill()));
+    for (int i = 0; i < numTeams; i +=2) {
       Match match = new Match(teams.get(i), teams.get(i + 1));
-      matches.add(match);
     }
-    return matches;
+
   }
 }
